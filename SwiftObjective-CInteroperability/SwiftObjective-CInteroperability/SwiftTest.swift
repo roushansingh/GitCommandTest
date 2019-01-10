ceff08afe292ff9d976f437d9e06b208181bc28d
roushan <roushan83mca@gmail.com>
2019-01-10 18:45:36 +0530

    Added new swift file

---
+++ SwiftObjective-CInteroperability/SwiftObjective-CInteroperability/SwiftTest.swift
@@ -0,0 +1,13 @@
+//
+//  SwiftTest.swift
+//  SwiftObjective-CInteroperability
+//
+//  Created by Chetu on 10/01/19.
+//  Copyright © 2019 Chetu. All rights reserved.
+//
+
+import UIKit
+
+class SwiftTest: NSObject {
+
+}
